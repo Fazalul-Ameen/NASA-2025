380177f41cf9c4497616c1ef638bc893da2505cc
Fazalul-Ameen <fazalulameen777@gmail.com>
2025-10-03 23:29:35 +0530

    Making it responsive

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// src/App.js - Minor Change
 import { Routes, Route, useLocation } from "react-router-dom";
 import SpaceBackground from "./components/SpaceBackground";
 import Header from "./components/Header";
@@ -10,27 +11,25 @@ import AboutPage from "./components/AboutPage";
 import "./App.css";
 
 function App() {
-  const location = useLocation(); 
+  const location = useLocation();
+  const isSimulationPage = location.pathname === "/simulation";
 
   return (
-    <div className="app-container">
-      { location.pathname !== "/simulation" && <SpaceBackground />}
+    // Add a class for the simulation page for specific styling
+    <div className={isSimulationPage ? "simulation-layout" : ""}>
+      { !isSimulationPage && <SpaceBackground />}
       <div className="overlay-content">
-        <Header style={
-          location.pathname === "/simulation" ? {
-            position: "absolute",
-            width: "100vw",
-            top: 0,
-          } : {}
-        } />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/asteroids" element={<Asteroids />} />
-          <Route path="/meteors" element={<Meteors />} />
-          <Route path="/simulation" element={<Simulation />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-        { location.pathname !== "/simulation" && <Footer />}
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/asteroids" element={<Asteroids />} />
+            <Route path="/meteors" element={<Meteors />} />
+            <Route path="/simulation" element={<Simulation />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
+        { !isSimulationPage && <Footer />}
       </div>
     </div>
   );
--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -1,6 +1,7 @@
+import "./Simulation.css";
 function Simulation() {
     return (
-        <iframe title="simulation" src="https://heckmon.github.io/nasa-hackathon/" style={{ width: "100%", height: "100vh", border: "none" }}/>
+        <iframe title="simulation" src="https://heckmon.github.io/nasa-hackathon/" className="simulation-frame" style={{ width: "100%", height: "100vh", border: "none" }}/>
     );
 }
 
