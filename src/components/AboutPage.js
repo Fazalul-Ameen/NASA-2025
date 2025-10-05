2e61d536fe12efca7e65fe5aae2daa09f801b3b2
Fazalul-Ameen <fazalulameen777@gmail.com>
2025-10-05 08:02:10 +0530

    Update AboutPage description for clarity and conciseness

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -8,7 +8,7 @@ export default function AboutPage() {
       <h2>About Meteor Madness</h2>
       <p>
         Meteor Madness is a NASA Space Apps Challenge project that visualizes meteors and asteroid
-        data in an interactive web interface. Explore trajectories, historical impacts, and live
+        data in an interactive web interface. Explore trajectories and live
         data sourced from NASA.
       </p>
       <Link to="https://www.spaceappschallenge.org/2025/find-a-team/lunarway/?tab=members" className="explore-btn">Know more about us..</Link>
